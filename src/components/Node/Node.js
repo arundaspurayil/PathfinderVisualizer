77620cb1276aba15b581a0c6cffb22b29593b085
Arundas Purayil <arundas0purayil@gmail.com>
2020-05-22 11:14:41 -0400

    Added styling for start node and goal node

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import './Node.css'
 
-function Node() {
-    return <div className="node" />
+function Node(props) {
+    const { properties } = props
+
+    const propertyClassName = properties.isStart
+        ? 'node-start'
+        : properties.isGoal
+        ? 'node-goal'
+        : ''
+
+    return <div className={`node ${propertyClassName}`} />
 }
 export default Node
--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react'
 import Node from './Node/Node'
 import './Visualizer.css'
 
+const ROWS = 20
+const COLUMNS = 50
+
+const startNodeRow = 9
+const startNodeCol = 10
+const goalNodeRow = 9
+const goalNodeCol = 30
+
 function Visualizer() {
-    const ROWS = 20
-    const COLUMNS = 50
     const [grid, setGrid] = useState([])
 
     useEffect(() => {
@@ -12,23 +18,35 @@ function Visualizer() {
         for (let row = 0; row < ROWS; row++) {
             let currentRow = []
             for (let col = 0; col < COLUMNS; col++) {
-                currentRow.push([])
+                currentRow.push(createNode(row, col))
             }
             nodes.push(currentRow)
         }
         setGrid(nodes)
     }, [])
 
+    function createNode(row, col) {
+        return {
+            row,
+            col,
+            isStart: row === startNodeRow && col === startNodeCol,
+            isGoal: row === goalNodeRow && col === goalNodeCol,
+            distance: Infinity,
+            isVisited: false,
+            isWall: false,
+            previousNode: null,
+        }
+    }
+
     const displayGrid = grid.map((row, rowIdx) => {
         return (
             <div className="row" key={rowIdx}>
                 {row.map((node, nodeIdx) => {
-                    return <Node key={rowIdx + nodeIdx} />
+                    return <Node key={rowIdx + nodeIdx} properties={node} />
                 })}
             </div>
         )
     })
-    console.log(displayGrid)
     return <div className="grid">{displayGrid}</div>
 }
 export default Visualizer
